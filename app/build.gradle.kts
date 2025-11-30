7d605d09af0ea1a238a7f9c768577440b6970b96
blair <me@blai.run>
2025-11-29 16:24:39 -0800

    Bump version to v1.1.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.air.pong"
         minSdk = 26
         targetSdk = 34
-        versionCode = 2
-        versionName = "1.0.4"
+        versionCode = 3
+        versionName = "1.1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
@@ -115,3 +115,4 @@ kotlin {
         jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_21)
     }
 }
+
